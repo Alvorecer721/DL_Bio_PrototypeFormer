6aa88a699f0aaa0001c14dcf96cab384210206e9
ris-iris <sagitova.ma@phystech.edu>
2023-12-02 20:08:01 +0100

    modifications

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 cd fewshotbench
 
-python run.py exp.name='prot_no_backbone_1_layer' method=protoformer dataset=swissprot lr=7e-7 +protoformer_is_feature=1 +protoformer_layer_num=1 
-python run.py exp.name='prot_no_backbone_3_layers' method=protoformer dataset=swissprot lr=7e-7 +protoformer_is_feature=1 +protoformer_layer_num=3
+python run.py exp.name='prot_no_backbone_1_layer' method=protoformer dataset=swissprot lr=1e-5 +protoformer_is_feature=1 +protoformer_layer_num=1 
+python run.py exp.name='prot_no_backbone_3_layers' method=protoformer dataset=swissprot lr=1e-5 +protoformer_is_feature=1 +protoformer_layer_num=3
 python run.py exp.name='prot_fcn_backbone_1_layer' method=protoformer dataset=swissprot lr=1e-4 +protoformer_is_feature=0 +protoformer_layer_num=1 
+python run.py exp.name='prot_fcn_backbone_3_layers' method=protoformer dataset=swissprot lr=1e-4 +protoformer_is_feature=0 +protoformer_layer_num=3
