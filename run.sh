e572a9c0b6a87d0dd977a286c53e43a4ee1ae80c
ris-iris <sagitova.ma@phystech.edu>
2023-12-03 10:54:52 +0100

    new run

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 cd fewshotbench
 
-python run.py exp.name='prot_no_backbone_1_layers_1_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=3e-4 +contrastive_coef=1 +protoformer_layer_num=1
-python run.py exp.name='prot_no_backbone_1_layers_0_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=3e-4 +contrastive_coef=0 +protoformer_layer_num=1 
-python run.py exp.name='prot_no_backbone_1_layers_0.1_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=3e-4 +contrastive_coef=0.1 +protoformer_layer_num=1 
+python run.py exp.name='prot_no_backbone_1_layers_1_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=5e-5 +contrastive_coef=1 +protoformer_layer_num=1
+python run.py exp.name='prot_no_backbone_1_layers_0_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=5e-5 +contrastive_coef=0 +protoformer_layer_num=1 
+python run.py exp.name='prot_no_backbone_1_layers_0.1_contrastive_coef_lr_5e-5' method=protoformer dataset=swissprot_no_backbone lr=5e-5 +contrastive_coef=0.1 +protoformer_layer_num=1 
