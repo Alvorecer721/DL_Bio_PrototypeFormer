90e386e6b43014e1e69faff092c94003d0c37a3b
ris-iris <sagitova.ma@phystech.edu>
2023-12-07 12:47:12 +0100

    change weight decay

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 cd fewshotbench
 
-python run.py exp.name='muris_fcn_1_layers_1_contrastive_coef_lr_1e-3_l2_reg' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=1 +protoformer_layer_num=1 weight_decay=0.01
-python run.py exp.name='muris_fcn_1_layers_0_contrastive_coef_lr_1e-3_l2_reg' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=0 +protoformer_layer_num=1 weight_decay=0.01
-python run.py exp.name='muris_fcn_1_layers_0.1_contrastive_coef_lr_1e-3_l2_reg' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=0.1 +protoformer_layer_num=1 weight_decay=0.01
+python run.py exp.name='prot_fcn_1_layers_1_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=swissprot lr=1e-3 +contrastive_coef=1 +protoformer_layer_num=1 weight_decay=0.01
+python run.py exp.name='prot_fcn_1_layers_0_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=swissprot lr=1e-3 +contrastive_coef=0 +protoformer_layer_num=1 weight_decay=0.01
+python run.py exp.name='prot_fcn_1_layers_0.1_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=swissprot lr=1e-3 +contrastive_coef=0.1 +protoformer_layer_num=1 weight_decay=0.01
+
+python run.py exp.name='muris_fcn_1_layers_1_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=1 +protoformer_layer_num=1 weight_decay=0.01
+python run.py exp.name='muris_fcn_1_layers_0_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=0 +protoformer_layer_num=1 weight_decay=0.01
+python run.py exp.name='muris_fcn_1_layers_0.1_contrastive_coef_lr_1e-3_l2_reg_0.01' method=protoformer dataset=tabula_muris lr=1e-3 +contrastive_coef=0.1 +protoformer_layer_num=1 weight_decay=0.01
