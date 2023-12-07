1d51a2443fb55b72c8cfa84f8caa9ac486596bef
ris-iris <sagitova.ma@phystech.edu>
2023-12-07 10:17:03 +0100

    tabula muris

--- run.sh
+++ run.sh
@@ -2,10 +2,6 @@
 
 cd fewshotbench
 
-python run.py exp.name='prot_fcn_1_layers_1_contrastive_coef_lr_1e-4' method=protoformer dataset=swissprot lr=1e-4 +contrastive_coef=1 +protoformer_layer_num=1
-python run.py exp.name='prot_fcn_1_layers_0_contrastive_coef_lr_1e-4' method=protoformer dataset=swissprot lr=1e-4 +contrastive_coef=0 +protoformer_layer_num=1 
-python run.py exp.name='prot_fcn_1_layers_0.1_contrastive_coef_lr_1e-4' method=protoformer dataset=swissprot lr=1e-4 +contrastive_coef=0.1 +protoformer_layer_num=1 
-
 python run.py exp.name='muris_fcn_1_layers_1_contrastive_coef_lr_1e-4' method=protoformer dataset=tabula_muris lr=1e-4 +contrastive_coef=1 +protoformer_layer_num=1
 python run.py exp.name='muris_fcn_1_layers_0_contrastive_coef_lr_1e-4' method=protoformer dataset=tabula_muris lr=1e-4 +contrastive_coef=0 +protoformer_layer_num=1 
 python run.py exp.name='muris_fcn_1_layers_0.1_contrastive_coef_lr_1e-4' method=protoformer dataset=tabula_muris lr=1e-4 +contrastive_coef=0.1 +protoformer_layer_num=1 
