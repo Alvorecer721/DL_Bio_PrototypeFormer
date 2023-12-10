1a9f08589bf04a05afce7bf428514301cde905f8
ris-iris <sagitova.ma@phystech.edu>
2023-12-10 13:22:18 +0100

    clean up protbert_embeddings.py

--- protbert_embeddings.py
+++ protbert_embeddings.py
@@ -6,23 +6,23 @@ import re
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-original_pt_folder = "./fewshotbench/data/swissprot/embeds/"  
+'''original_pt_folder = "./fewshotbench/data/swissprot/embeds/"  
 subset_fasta_path = "subset.fasta"  
 
 pt_files = [file for file in os.listdir(original_pt_folder) if file.endswith(".pt")]
 
 protein_ids = [file.split('.')[0] for file in pt_files]
-
+'''
 '''
     Because the original embedding did not use all the proteins in the dataset, 
     we use the same protein id as the original embedding to generate the new embedding using protbert.
     The used protein sequences are stored in the file subset_fasta.fasta.
 '''
 
-with open("./fewshotbench/data/swissprot/uniprot_sprot.fasta", "r") as original_fasta, open(subset_fasta_path, "w") as subset_fasta:
+'''with open("./fewshotbench/data/swissprot/uniprot_sprot.fasta", "r") as original_fasta, open(subset_fasta_path, "w") as subset_fasta:
     records = [record for record in SeqIO.parse(original_fasta, "fasta") if record.id.split("|")[1] in protein_ids]
     SeqIO.write(records, subset_fasta, "fasta")
-
+'''
 
 tokenizer = BertTokenizer.from_pretrained("Rostlab/prot_bert", do_lower_case=False)
 model = BertModel.from_pretrained("Rostlab/prot_bert").to(device)
@@ -36,7 +36,7 @@ with open("subset.fasta", "r") as fasta_file:
     while line != '':
         if line.startswith(">"):  
             protein_id = line.split("|")[1]
-
+            print(protein_id)
             sequence_line = ''
             line = fasta_file.readline().strip()
             while line != '' and not line.startswith(">"):
@@ -49,6 +49,7 @@ with open("subset.fasta", "r") as fasta_file:
             output = model(**encoded_input)
 
             embedding = output["last_hidden_state"].mean(dim=1).cpu().detach()
+            print(embedding)
 
             torch.save({"embedding": embedding}, f"{save_path}/{protein_id}.pt")
             
