9b1adad39075b6b8b0497e9e0977140911bde521
ris-iris <sagitova.ma@phystech.edu>
2023-12-10 12:59:54 +0100

    fix protbert

--- protbert_embeddings.py
+++ protbert_embeddings.py
@@ -30,7 +30,7 @@ model = BertModel.from_pretrained("Rostlab/prot_bert").to(device)
 save_path = "./fewshotbench/data/swissprot/protbert_emb"
 os.makedirs(save_path, exist_ok=True)
 
-print("start generating")
+print(device)
 
 with open("subset.fasta", "r") as fasta_file:
     line = fasta_file.readline().strip()
